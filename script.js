dd57663c148fd4e16727c35268158f80936536a4
o-lat <omarl1933@gmail.com>
2017-05-11 15:08:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ names = [];
 
 function addName() {
 	if (document.getElementById('name').value == "") {
-		alert('Textbox blank.');
-	} else {		
+		alert('Please enter a name.');
+	} else {
 		names.push(document.getElementById('name').value);
 		console.log(names);
+		document.getElementById('output').style.display = 'block';
+		getList();
+		document.getElementById('name').focus();
+		document.getElementById('name').value = '';
 	}
 }
 function getWinner() {
 	var winner = Math.floor((Math.random() * names.length) + 0);
-	console.log('the winner is:' + names[winner]);
-	getList()
+	document.getElementById('winner').innerHTML = names[winner] + ' is the winner!';
+	
 }
+
 function getList() {
-	for (i in names){
-		String(document.getElementById('names').innerHTML = names).split('\n');
+	var outputNames;
+	outputNames = "<ul>";
+
+	for (i = 0; i < names.length; i++) {
+		outputNames += "<li>" + names[i] + "</li>";
 	}
+	outputNames += "</ul>";
+	document.getElementById("names").innerHTML = outputNames;
 }
